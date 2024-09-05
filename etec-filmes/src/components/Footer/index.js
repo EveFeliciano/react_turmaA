8b95769e2b7c65e1d0feba2bc870cc82eba37376
EveFeliciano <evellynsanfeliciano@gmail.com>
2024-09-05 11:15:16 -0300

    atualizacoes

---
+++ etec-filmes/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>EBUPOP! &copy; Desenvolvido por eburim</h2>
+        </footer>
+    );
+}
+
+export default Footer;
